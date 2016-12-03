05f4752dc090c1d2bfa0f1a8658e8f06708b0288
riffmichael <riffms01@students.ipfw.edu>
2016-12-02 19:28:36 -0500

    Update candy.cs
    added board object

--- proto2/candy.cs
+++ proto2/candy.cs
@@ -1,76 +1,102 @@
 using System;
-					
-namespace test
-{
 
-	public class Candy {
+public class Candy
+{
 	int cValue;
 	bool isChecked;
 	int location;
-		
-		public Candy(){
-		this.cValue=0;
+	public Candy()
+	{
+		this.cValue = 0;
 		this.isChecked = false;
 		this.location = 0;
-		}
-		
-		public bool isClicked()  {
-			return this.isChecked;			
-		
-		}
-		public void setClicked() {
-		this.isChecked = true;
-		}
-		public void setNotClicked() {
-		this.isChecked = false;
-		}
-		public int showLocation() {
-		return this.location;
-		}
-		public void setLocation(int loc) {
-		this.location = loc;
-		}
-	
-		public void setValue(int val) {
-		this.cValue = val;
-		}
-		public int getValue() {
-		return this.cValue;
-		}
-	
-	
+	}
 
-	
-	
+	public bool isClicked()
+	{
+		return this.isChecked;
+	}
+
+	public void setClicked()
+	{
+		this.isChecked = true;
+	}
+
+	public void setNotClicked()
+	{
+		this.isChecked = false;
+	}
+
+	public int showLocation()
+	{
+		return this.location;
+	}
+
+	public void setLocation(int loc)
+	{
+		this.location = loc;
+	}
+
+	public void setValue(int val)
+	{
+		this.cValue = val;
+	}
+
+	public int getValue()
+	{
+		return this.cValue;
+	}
+} //class candy
+
+public class Board
+{
+	static int candysize = 64;
+	Candy[] newBoard = new Candy[candysize];
+	public Board()
+	{
+		for (int i = 0; i < candysize; i++)
+		{
+			newBoard[i] = new Candy();
+			newBoard[i].setValue(0);
+			newBoard[i].setNotClicked();
+		}
+	}
+
+	public int getBoardSize()
+	{
+		return candysize;
+	}
+
+	public Candy getCandy(int i)
+	{
+		return newBoard[i];
+	}
+} //class Board
+
+public class program
+{
 	public void Main()
 	{
-		Console.WriteLine("Hello World");
-		
-		int candysize = 64;
-		
-		Candy[] newCandy = new Candy[candysize];
-		
-		int[] values = new int[candysize];
-		
-		for (int i = 0; i < candysize; i++){
-		
-		newCandy[i] = new Candy();
-		newCandy[i].setValue(i%4);
+		board newboard = new Board();
+		Console.WriteLine(newboard.getCandy(0).isClicked());
+		newboard.getCandy(0).setClicked();
+		Console.WriteLine(newboard.getCandy(0).isClicked());
+		for (int i = 0; i < newboard.getBoardSize(); i++)
+		{
+			Console.Write(newboard.getCandy(i).getValue() + " ");
 		}
-		
-		
-		Console.WriteLine(newCandy[0].isClicked());
-		newCandy[0].setClicked();
-		Console.WriteLine(newCandy[0].isClicked());
-		
-		for (int i = 0; i < candysize; i++){
-		
-		Console.Write(newCandy[i].getValue()+" ");
+
+		Console.WriteLine();
+		for (int i = 0; i < newboard.getBoardSize(); i++)
+		{
+			if (newboard.getCandy(i).isClicked())
+			{
+				Console.Write("1 ");
+			}
+			else
+			{
+				Console.Write("0 ");
+			}
 		}
-	
-		
-		
-	
-	}
-	}
-}
+	} //main()
+} //class program
